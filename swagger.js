ac2ec33bd932c693d284222bd508855246db4dec
Harrison Merrill <hwalkermerrill@gmail.com>
2026-05-23 22:33:57 +0900

    create swagger files

---
+++ swagger.js
@@ -0,0 +1,15 @@
+const swaggerAutogen = require("swagger-autogen")();
+
+const doc = {
+	info: {
+		title: "Cooking Merrill(y) API",
+		description: "API documentation for the Cooking Merrill(y) project"
+	},
+	host: "cooking-merrilly.onrender.com",
+	schemes: ["https"]
+};
+
+const outputFile = "./swagger-output.json";
+const endpointsFiles = ["./src/controllers/routes.js"];
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
