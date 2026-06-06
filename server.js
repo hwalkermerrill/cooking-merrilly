3322c925009a004aa27beb2156721f5a11df9b70
Harrison Merrill <hwalkermerrill@gmail.com>
2026-06-06 22:37:59 +0900

    continuing to try to fix swagger xD

--- server.js
+++ server.js
@@ -55,11 +55,14 @@ app.use(devLogs);
 
 // Swagger Docs
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile, {
-	oauth: {
-		clientId: process.env.GOOGLE_CLIENT_ID,
-		clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-		appName: "Cooking Merrill(y) API",
-		usePkceWithAuthorizationCodeGrant: true
+	swaggerOptions: {
+		oauth2RedirectUrl: "https://cooking-merrilly.onrender.com/api-docs/oauth2-redirect.html",
+		oauth: {
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			appName: "Cooking Merrill(y) API",
+			usePkceWithAuthorizationCodeGrant: true
+		}
 	}
 }));
 
@@ -70,6 +73,5 @@ app.use("/", require("./src/routes"));
 mongoose.connection.once("open", () => {
 	app.listen(PORT, () => {
 		console.log(`Server is running on http://127.0.0.1:${PORT}`);
-		console.log(`GOOGLE_CLIENT_ID: ${process.env.GOOGLE_CLIENT_ID}`);
 	});
 });
